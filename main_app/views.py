a75013b0cca2a1e3ab502717901f689c27bf82bc
charlenebatiste <batistecharlene@gmail.com>
2021-06-23 16:20:18 -0400

    added: partials, superuser, migrated

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
 
@@ -8,20 +9,12 @@ def about(request):
 def index(request):
   return render(request, 'index.html')
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
-
 def cats_index(request):
-    data = {
-        'cats': cats
-    }
+    cats = Cat.objects.all()
+    data = { 'cats': cats }
     return render(request, 'cats/index.html', data)
+
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    data = { 'cat': cat }
+    return render(request, 'cats/show.html', data)
